b030e9e747bf44a7bc1ede8da51162511b887874
Abhay <yabhay@iiitmanipur.ac.in>
2026-05-15 18:36:42 +0530

    corporate geek gst and cin and availibilty

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -2,4 +2,6 @@
 
 // export const url = "http://localhost:4002/api/";
 
-export const url = "https://god-backend.vercel.app/api/";
+// export const url = "https://god-backend.vercel.app/api/";
+
+export const url = "https://god-test-backend-afewhbghcfcuc9eq.southindia-01.azurewebsites.net/api/";
