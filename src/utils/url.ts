8c2a8553eb688867e71f3f5243511ab9aad6f0f7
Abhay <yabhay@iiitmanipur.ac.in>
2026-06-23 06:42:59 +0530

    removed test .yml file

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -3,5 +3,5 @@ export const url = "https://god-backend-e2hed5fcaxcwgchs.southindia-01.azurewebs
 // export const url = "http://localhost:4002/api/";
 
 // export const url = "https://god-backend.vercel.app/api/";
- 
+
 // export const url = "https://god-test-backend-afewhbghcfcuc9eq.southindia-01.azurewebsites.net/api/";
