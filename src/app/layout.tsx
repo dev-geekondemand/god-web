2d77535f93bc9727041183c3581b80ace68bc650
Abhay <yabhay@iiitmanipur.ac.in>
2026-07-13 12:06:30 +0530

    clarity script added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -179,6 +179,16 @@ export default function RootLayout({
         </main>
         <Footer />
         </ReduxProvider>
+
+        <Script id="clarity-analytics" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "xf0ghd25az");
+          `}
+        </Script>
       </body>
     </html>
   );
